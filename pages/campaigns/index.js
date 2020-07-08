0195195ea18970b5864f85ffb9c9c868b74faafc
Twice2020 <67903309+Twice2020@users.noreply.github.com>
2020-07-08 13:39:20 +0100

    Update index.js

--- pages/campaigns/index.js
+++ pages/campaigns/index.js
@@ -2,3 +2,11 @@
 
 import redirect from "nextjs-redirect"
 export default redirect("https://www.twice-agency.com/")
+<script>
+  window.addEventlistener('load',function(){
+    if(window.location.pathname == "/campaigns/riviera/french/accommodation"){
+      gtag('event', 'conversion', {'send_to': 'AW-763469410/eUAvCMnt6NUBEOK8huwC'});
+    }
+  })
+</script>
+
